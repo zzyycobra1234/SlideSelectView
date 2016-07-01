035e8628172a63070ad8a4ca824486595fc5a725
zhouyi <291436485@qq.com>
2016-07-01 15:51:37 +0800

    修改新的样式

--- app/src/main/java/com/socks/slideselectview/MainActivity.java
+++ app/src/main/java/com/socks/slideselectview/MainActivity.java
@@ -2,7 +2,6 @@ package com.socks.slideselectview;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,16 +18,16 @@ public class MainActivity extends ActionBarActivity {
 
 		slideSelectView = (SlideSelectView) findViewById(R.id.slideSelectView);
 
-		textStrings = new String[]{"汇率 1.1234", "汇率 1.1334", "汇率 1.1434", "汇率 1.1534",
-				"汇率 1.1734"};
-		slideSelectView.setString(textStrings);
+//		textStrings = new String[]{"汇率 1.1234", "汇率 1.1334", "汇率 1.1434", "汇率 1.1534",
+//				"汇率 1.1734"};
+//		slideSelectView.setString(textStrings);
 
-		slideSelectView.setOnSelectListener(new SlideSelectView.onSelectListener() {
-			@Override
-			public void onSelect(int index) {
-				Toast.makeText(MainActivity.this, textStrings[index], Toast.LENGTH_SHORT).show();
-			}
-		});
+//		slideSelectView.setOnSelectListener(new SlideSelectView.onSelectListener() {
+//			@Override
+//			public void onSelect(int index) {
+//				Toast.makeText(MainActivity.this, textStrings[index], Toast.LENGTH_SHORT).show();
+//			}
+//		});
 	}
 
 }
